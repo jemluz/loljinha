b54e2169f71f95b18db50b328178943df29d1be6
Jemima <contatojemima@gmail.com>
2019-04-02 07:35:29 -0300

    configurações de conexao

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,44 +1,44 @@
 // Update with your config settings.
-
+//  esse arquivo é responsável pelas configurações de conexão c o banco de dados.
+//  ele permite diferentes conxeções para diferentes ambientes (produção, desenvolvimento, testes...)
+//  nesse caso ficamos apenas com as confg de produção e dispensamos as demais
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+  // production: { 
+  client: 'postgresql',
+  connection: {
+    database: 'loljinha_db',
+    user:     'admin',
+    password: 'admin'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  pool: {
+    min: 2,
+    max: 10
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    tableName: 'knex_migrations'
   }
+  // }
 
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 };
