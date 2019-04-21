a52de5b8a5e8f516ad647a4555c99c0efcaf430a
Jemima <contatojemima@gmail.com>
2019-04-21 19:38:28 -0300

    correção das rotas (melhor identificação)

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,20 +1,20 @@
-// definição de rota para acessar os usuários pela url /users usando o metodo http POST.
+// definição de rota para acessar os usuários pela url /clientes usando o metodo http POST.
 module.exports = app => {
   app.route('/clientes')
-    .post(app.api.user.save)
-    .get(app.api.user.get)
+    .post(app.api.cliente.saveCliente)
+    .get(app.api.cliente.getCliente)
   // .post vai associar qual metodo será chamado quando ele receber uma requisição nessa rota usando post.
-  // consign usa o caminho app.api.user.save para chamar a instancia, acessar a api, entrar no arquivo user e pegar a função save retornada pelo modulo.
+  // consign usa o caminho app.api.cliente.save para chamar a instancia, acessar a api, entrar no arquivo cliente e pegar a função save retornada pelo modulo.
 
-  app.route('/clientes/:id')
-    .put(app.api.user.save)
-    .put(app.api.user.getById)
+  app.route('/clientes/:login')
+    .put(app.api.cliente.saveCliente)
+    .get(app.api.cliente.getClienteByLogin)
   // o método save serve tanto para inserir quanto para alterar um usuário, por isso a diferença entre o POST e o PUT está no parâmetro fornecido na url (no caso o :id ou a ausencia dele), é assim que o método descobri qual tipo de requisição está sendo feita
 }
 
 // um exemplo da mesma configuração, sem usar o consign:
-// const user = require('../api.user')
+// const cliente = require('../api.cliente')
 // module.exports = app => {
-//   app.route('/users')
-//     .post(user.save)
+//   app.route('/clientes')
+//     .post(cliente.save)
 // }
