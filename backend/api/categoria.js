9dcd8ce528fa926c6664f08da6d5e56dcbcc88ac
Jemima <contatojemima@gmail.com>
2019-04-21 19:38:50 -0300

    vamos começar as api obg

--- backend/api/categoria.js
+++ backend/api/categoria.js
@@ -1,33 +1,33 @@
-module.exports = app => {
-  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+// module.exports = app => {
+//   const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
-  const save = (requisicao, resposta) => {
-    const category = { ...requisicao.body }
-    if (requisicao.params.id) category.id = requisicao.params.id
+//   const save = (requisicao, resposta) => {
+//     const category = { ...requisicao.body }
+//     if (requisicao.params.id) category.id = requisicao.params.id
 
-    try {
-      existsOrError(category.descricao, 'Categoria não informada.')
-    } catch (msg) {
-      return resposta.status(400).send(msg)
-    }
+//     try {
+//       existsOrError(category.descricao, 'Categoria não informada.')
+//     } catch (msg) {
+//       return resposta.status(400).send(msg)
+//     }
 
-    if (category.id) {
-      app.db('categoria')
-        .update(category)
-        .where({ id: category.id })
-        .then(_ => resposta.status(204).send())
-        .catch(err => resposta.status(500).send(err))
-    } else {
-      app.db('categoria')
-        .insert(category)
-        .then(_ => resposta.status(204).send())
-        .catch(err => resposta.status(500).send(err))
-    }
+//     if (category.id) {
+//       app.db('categoria')
+//         .update(category)
+//         .where({ id: category.id })
+//         .then(_ => resposta.status(204).send())
+//         .catch(err => resposta.status(500).send(err))
+//     } else {
+//       app.db('categoria')
+//         .insert(category)
+//         .then(_ => resposta.status(204).send())
+//         .catch(err => resposta.status(500).send(err))
+//     }
 
-    const remove = async (requisicao, resposta) => {
-      try {
+//     const remove = async (requisicao, resposta) => {
+//       try {
         
-      }
-    }
-  }
-}
+//       }
+//     }
+//   }
+// }
