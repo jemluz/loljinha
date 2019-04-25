45802b7a4b1e0a74e7f832469fb951f3b4224d37
Jemima <contatojemima@gmail.com>
2019-04-25 07:57:14 -0300

    autenticação provisórioa

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import store from './store'
+import router from './config/router'
+import store from './config/store'
+import msgs from './config/msgs'
 
 Vue.config.productionTip = false
 
 // TEMPORÁRIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6ImplbXMiLCJub21lIjoiamVtaW1hIiwiaWF0IjoxNTU2MTIwMjMyLCJleHAiOjE1NTYxMjAyOTJ9.yYdoGBHOMmWbHUVD5hhkYWMij1JFFtzcJi_rP71hB30'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbiI6ImplbXMiLCJub21lIjoiamVtaW1hIiwiaWF0IjoxNTU2MTMyNjAyLCJleHAiOjE1NTYzOTE4MDJ9.sKeHnYJ7hd7UDbQLsnXbHfhyd5lQcVOWPAWRYIzop2Q'
 
 new Vue({
   router,
